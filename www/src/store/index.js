1e75e94ab201a638d706fffc814c53eca253f7d4
Kyle Krawl <kyle.krawl@gmail.com>
2017-12-05 10:00:13 -0700

    Setup for deployment.

--- www/src/store/index.js
+++ www/src/store/index.js
@@ -4,13 +4,13 @@ import vuex from 'vuex'
 import router from 'router'
 
 let api = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: '/api/',
   timeout: 2000,
   withCredentials: true
 })
 
 let auth = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: '/',
   timeout: 2000,
   withCredentials: true
 })
